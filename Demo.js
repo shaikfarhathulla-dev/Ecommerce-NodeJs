0b70faded2c3da00b675ac4fd2513363dfa330c5
Shaik Farhathulla <shaikfarhath@Shaiks-MacBook-Air.local>
2025-09-13 21:34:54 +0530

    Initial Project commit

---
+++ Demo.js
@@ -0,0 +1,10 @@
+// npm init - intial the package.json
+// node server.js - run the server.js file and starts the server
+// npm install nodemon - nodemon is used for auto re-run the server after the changes
+// nodemon server.js - cmd to run
+
+
+function sayHello(){
+    console.log("Hello! Welcome here");
+}
+sayHello();
